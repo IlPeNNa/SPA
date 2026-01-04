2474afd154110e673556b8c63dec86978e9b8097
Alex Pennini <alex.pennini@edu.unife.it>
2026-01-04 17:24:08 +0100

    Lavorare su app component

---
+++ bowling_FE/src/app/conversazioni/home/home.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { SessionService } from '../../servizi/session.service';
+import { Utente } from '../../modelli/utente.models';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+
+  loggedUser: Utente | null = null;
+
+  constructor(private session: SessionService) {}
+
+  ngOnInit(): void {
+    this.loggedUser = this.session.getLoggedUser();
+  }
+
+}
